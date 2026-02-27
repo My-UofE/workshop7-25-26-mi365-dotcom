045f0d893218f6b620353f1d58cb8e715400fbd1
mi365-dotcom <mi365@exeter.ac.uk>
2026-02-27 15:48:04 +0000

    Polymorphisim

--- TestCylinder2.java
+++ TestCylinder2.java
@@ -3,12 +3,13 @@
  */
 public class TestCylinder2 {
     public static void main(String[] args) {
-       Cylinder cy2 = new Cylinder(5.0, 2.0);
+       Circle cy2 = new Cylinder(5.0, 2.0);
 
-       double h = cy2.getHeight();
+       double h = ((Cylinder)cy2).getHeight();
+       double v = ((Cylinder)cy2).getVolume();
        double r = cy2.getRadius();
        double a = cy2.getArea();
-       double v = cy2.getVolume();
+       
 
        System.out.printf("Height: %.2f, Radius: %.2f, Area: %.2f, Volume: %.2f\n", h, r, a, v);
 
